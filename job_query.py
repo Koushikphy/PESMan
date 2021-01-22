dde5017f8bb2230fb8ca4d8850ca077ba8bb0401
Koushik Naskar <koushik.naskar9@gmail.com>
2021-01-22 21:53:25 +0530

    up

--- job_query.py
+++ job_query.py
@@ -1,21 +1,14 @@
 from __future__ import print_function
-import paramiko 
+import paramiko,os, subprocess,time
 
 
-def getDetails(userName, ip, location, passwd=None):
+def getRemoteDetails(userName, ip, location, passwd=None):
 
-    print('='*90)
-    print("\033[1mHost : {}\nLocation : {}\n\033[0m".format(userName, location))
 
     client = paramiko.SSHClient()
     client._policy = paramiko.WarningPolicy()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-    # ssh_config = paramiko.SSHConfig()
-    # user_config_file = os.path.expanduser("~/.ssh/config")
-    # if os.path.exists(user_config_file):
-    #     with open(user_config_file) as f:
-    #         ssh_config.parse(f)
 
     client.connect(
         ip,
@@ -24,15 +17,31 @@ def getDetails(userName, ip, location, passwd=None):
         password=passwd
     )
 
-    _,b,_ = client.exec_command('cd {}; python PESMan.py status'.format(location))
+    for loc in location:
+        print('='*90)
+        print("\033[1mHost : {}\nLocation : {}\n\033[0m".format(userName, loc))
 
-    for i in b.readlines(): print(i,end='')
+        _,b,_ = client.exec_command('cd {}; python PESMan.py status'.format(loc))
+
+        for i in b.readlines(): print(i,end='')
 
 
-    _,b,_ = client.exec_command("""
-        python -c "import os;import time ;print(time.ctime(os.path.getmtime(os.path.expanduser('{}/h3.db'))))"
-    """.format(location))
-    bb = b.readline()
-    print("Database last modified on : ",bb)
-    print('-'*90,'\n')
+        _,b,_ = client.exec_command("""
+            python -c "import os;import time ;print(time.ctime(os.path.getmtime(os.path.expanduser('{}/h3.db'))))"
+        """.format(loc))
+        bb = b.readline()
+        print("Database last modified on : ",bb)
+        print('-'*90,'\n')
     client.close()
+
+
+
+
+
+
+def getLocalDetails(location):
+    currentDir = os.getcwd()
+    os.chdir(location)
+    subprocess.call('python PESMan.py status', shell=True)
+    print(time.ctime(os.path.getmtime('h3.db')))
+    os.chdir(currentDir)
